8b7bcc844b8f0b11487ebcfd9897ee622f078bc5
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2018-09-06 10:30:45 +0300

    Add BoardTest: WhenPlayerDropsTailAndTestLimitIsNotReached_ShouldMovePlayerCardToTestFromDev

--- FeaturebanGame/FeatureGame.Domain.Tests/BoardTests.cs
+++ FeaturebanGame/FeatureGame.Domain.Tests/BoardTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using FeaturebanGame.Domain;
 using FeatureGame.Domain.Tests.DSL;
@@ -8,6 +9,31 @@ namespace FeatureGame.Domain.Tests
     [TestFixture]
     public class BoardTests
     {
+        [Test]
+        public void WhenPlayerDropsTailAndTestLimitIsNotReached_ShouldMovePlayerCardToTestFromDev()
+        {
+            var board = Create.Board(@"
+| Backlog |  Dev     | Test     | Done |
+|         |  [MK  ]  |          |    0 |
+");
+            var mikhail = Create.Player
+                .WithName("MK")
+                .WithBoard(board)
+                .Please();
 
+            mikhail.Play(CoinDropResult.Tail);
+
+            AssertBoard(board, @"
+| Backlog |   Dev    |   Test   | Done |
+|         |          |  [MK  ]  |    0 |
+");
+        }
+
+        private static void AssertBoard(Board board, string expectedBoardSketch)
+        {
+            var expected = expectedBoardSketch.Replace("\r\n", "").Replace("\n", "").Replace("\t", "").Replace(" ", "");
+            var actual = board.ToString().Replace("\r\n", "").Replace("\n", "").Replace("\t", "").Replace(" ", "");
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
--- FeaturebanGame/FeatureGame.Domain.Tests/DSL/Create.cs
+++ FeaturebanGame/FeatureGame.Domain.Tests/DSL/Create.cs
@@ -1,8 +1,20 @@
-﻿namespace FeatureGame.Domain.Tests.DSL
+﻿using FeaturebanGame.Domain;
+
+namespace FeatureGame.Domain.Tests.DSL
 {
     public static class Create
     {
         public static PlayerBuilder Player => new PlayerBuilder();
         public static GameBuilder Game => new GameBuilder();
+
+        public static Board Board(string boardSketch)
+        {
+            return BoardFabric.CreateBoard(boardSketch);
+        }
+
+        public static Board Board(string boardSketch, ICoin coin)
+        {
+            return BoardFabric.CreateBoard(boardSketch, coin);
+        }
     }
 }
