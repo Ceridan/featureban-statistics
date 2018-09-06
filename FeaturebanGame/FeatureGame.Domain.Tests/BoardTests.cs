4ac1e00f460754fa0c637e354233be69466b265a
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2018-09-06 12:59:59 +0300

    Add BoardTests for block card situation

--- FeaturebanGame/FeatureGame.Domain.Tests/BoardTests.cs
+++ FeaturebanGame/FeatureGame.Domain.Tests/BoardTests.cs
@@ -49,6 +49,47 @@ namespace FeatureGame.Domain.Tests
 ");
         }
 
+        [Test]
+        public void WhenPlayerDropsHeadAndDevLimitIsNotReached_ShouldBlockTheCardAndPullTheNewOne()
+        {
+            var board = Create.Board(@"
+| Backlog |  Dev     | Test     | Done |
+|         |  [MK  ]  |          |    0 |
+");
+            var mikhail = Create.Player
+                .WithName("MK")
+                .WithBoard(board)
+                .Please();
+
+            mikhail.Play(CoinDropResult.Head);
+
+            AssertBoard(board, @"
+| Backlog |   Dev    |   Test   | Done |
+|         |  [MK B]  |          |    0 |
+|         |  [MK  ]  |          |      |
+");
+        }
+
+        [Test]
+        public void WhenPlayerDropsHeadAndDevLimitIsReached_ShouldBlockTheCard()
+        {
+            var board = Create.Board(@"
+| Backlog |  Dev (1) | Test (1) | Done |
+|         |  [MK  ]  |          |    0 |
+");
+            var mikhail = Create.Player
+                .WithName("MK")
+                .WithBoard(board)
+                .Please();
+
+            mikhail.Play(CoinDropResult.Head);
+
+            AssertBoard(board, @"
+| Backlog |  Dev (1) | Test (1) | Done |
+|         |  [MK B]  |          |    0 |
+");
+        }
+
         private static void AssertBoard(Board board, string expectedBoardSketch)
         {
             var expected = expectedBoardSketch.Replace("\r\n", "").Replace("\n", "").Replace("\t", "").Replace(" ", "");
